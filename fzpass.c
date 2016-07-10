d8c61fadd1434525c926ea10f286af2670dba0f1
KeyboardFire <andy@keyboardfire.com>
2016-07-10 22:28:53 +0900

    Make it actually functional

--- fzpass.c
+++ fzpass.c
@@ -1,26 +1,112 @@
-#include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char* argv[]) {
-    char *line = NULL;
-    size_t bufsize = 0;
-    ssize_t len;
-    while ((len = getline(&line, &bufsize, stdin)) != -1) {
+#include <unistd.h>
+#include <ftw.h>
+#include <ncurses.h>
+#include <string.h>
+
+char **passwords = NULL;
+int nPasswords = 0;
+int addPassword(const char *fpath, const struct stat *sb, int typeflag) {
+    if (typeflag == FTW_F && !strstr(fpath, "/.git/") && strstr(fpath, ".gpg")) {
+        passwords = realloc(passwords, (++nPasswords) * sizeof *passwords);
+        int passwordLen = strlen(fpath) - 5;
+        passwords[nPasswords - 1] = malloc(passwordLen * sizeof *fpath);
+        strncpy(passwords[nPasswords - 1], fpath + 2, passwordLen);
+        passwords[nPasswords - 1][passwordLen - 1] = '\0';
+    }
+    return 0;
+}
+
+int keypress(WINDOW *typeWin, WINDOW *outputWin, char ch, char *buf) {
+    char *p = buf;
+    for (; *p; ++p);
+
+    switch (ch) {
+    case 4: // ^D
+        return 0;
+    case 127: // ^? (backspace)
+    case '\b':
+        if (*buf) {
+            waddstr(typeWin, "\b \b");
+            *(p-1) = '\0';
+        }
+        break;
+    default:
+        waddch(typeWin, ch);
+        *p = ch;
+    }
+
+    int numPrinted = 0;
+    werase(outputWin);
+    box(outputWin, 0, 0);
+    for (int i = 0; i < nPasswords; ++i) {
+        char *password = passwords[i];
+        int pos = 0, npat = 0, len = strlen(password);
         int *highlight = calloc(len, sizeof *highlight);
-        for (int i = 1; i < argc; ++i) {
-            int pos = 0;
-            for (char *pat = argv[i]; *pat; ++pat) {
-                for(; pos < len && line[pos] != *pat; ++pos);
+        for (char *pat = buf; *pat; ++pat) {
+            if (*pat == ' ') {
+                pos = 0;
+                ++npat;
+            } else {
+                for (; pos < len && password[pos] != *pat; ++pos);
                 if (pos == len) goto skipprint;
-                highlight[pos++] = 30 + i;
+                highlight[pos++] = npat + 1;
             }
         }
+        if (numPrinted > LINES - 6) break;
+        wmove(outputWin, ++numPrinted, 1);
         for (int i = 0; i < len; ++i) {
-            if (highlight[i]) printf("\e[1m");
-            printf("\e[%dm%c", highlight[i], line[i]);
+            wattrset(outputWin, highlight[i] ?
+                    A_BOLD | COLOR_PAIR(highlight[i]) :
+                    A_NORMAL);
+            waddch(outputWin, password[i]);
         }
 skipprint:
         free(highlight);
     }
-    free(line);
+    wattrset(outputWin, A_NORMAL);
+    wrefresh(outputWin);
+    wrefresh(typeWin);
+
+    return 1;
+}
+
+int main(int argc, char* argv[]) {
+    initscr();
+    raw();
+    noecho();
+
+    start_color();
+    for (int i = 1; i <= 7; ++i) {
+        init_pair(i, i, COLOR_BLACK);
+    }
+
+    WINDOW *outputWin = newwin(LINES - 3, COLS, 3, 0);
+
+    WINDOW *typeWin = newwin(3, COLS, 0, 0);
+    refresh();
+    box(typeWin, 0, 0);
+    wmove(typeWin, 1, 1);
+    wrefresh(typeWin);
+
+    char *passDir;
+    char *passEnvDir = getenv("PASSWORD_STORE_DIR");
+    if (passEnvDir) passDir = passEnvDir;
+    else {
+        char *homeDir = getenv("HOME");
+        passDir = malloc((strlen(homeDir) + 18) * sizeof *passDir);
+        sprintf(passDir, "%s/.password-store/", homeDir);
+    }
+    chdir(passDir);
+    if (ftw(".", addPassword, 15) == -1) {
+        perror("ftw");
+        return 1;
+    }
+
+    char *buf = calloc(512, sizeof *buf);
+    keypress(typeWin, outputWin, '\b', buf);
+    while (keypress(typeWin, outputWin, getch(), buf));
+
+    endwin();
     return 0;
 }
